b4423d77500e7077e30268b64dd6ea364647c175
1431KIRTI <99132033+1431KIRTI@users.noreply.github.com>
2022-02-22 22:50:47 +0530

    gives output

---
+++ grade.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void){
+int num;
+printf("Enter your mark ");
+scanf("%d",&num);
+printf(" You entered %d", num); // printing outputs
+
+	if(num is greater than 84 and less than or equal to 100){
+	printf(" grade A"); // printing outputs
+		}
+	else if ( num is greater than 69 and less than or equal to 84){ // Note the space between else & if
+		printf(" grade B");
+		}
+	else if ( num is greater than 54 and less than or equal to 69){
+		printf(" grade C");
+		}
+	else if ( num is greater than 39 and less than or equal to 54){
+		printf(" grade D");
+		}
+  else if ( num is below 40){
+		printf(" grade F");
+    
+return 0;
+}
